0d1d4cbc5e63a37261aa602f6d190e3eac406334
jhelum13 <jngelinas@yahoo.com>
2021-12-03 15:24:49 -0800

    fixed venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\jngel\Documents\Code\Final-Project-Pathfinder-2e\PF2eApp\venv
+set VIRTUAL_ENV=D:\jngel\Documents\Code\Final-Project-Pathfinder-2e\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
